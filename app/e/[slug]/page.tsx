95c25d408c5e6bcfe8b0f57fee19e4d5507e047f
ASBrasil <contato@asbrasil.tur.br>
2026-08-12 00:42:23 -0300

    Update page.tsx

--- app/e/[slug]/page.tsx
+++ app/e/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import { db } from "@/lib/db";
 import { notFound } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function EventPage({ params }: { params: { slug: string } }) {
   const event = await db.event.findUnique({
     where: { slug: params.slug },
